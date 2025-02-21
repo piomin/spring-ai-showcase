bb7d7ecd8a3361c0bb084747ecc541018579322d
piomin <piotr.minkowski@gmail.com>
2025-02-21 10:35:58 +0100

    Adding RAG (2)

--- src/main/java/pl/piomin/services/controller/StockController.java
+++ src/main/java/pl/piomin/services/controller/StockController.java
@@ -1,5 +1,7 @@
 package pl.piomin.services.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
@@ -10,26 +12,20 @@ import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.document.Document;
-import org.springframework.ai.openai.OpenAiEmbeddingOptions;
+import org.springframework.ai.rag.preretrieval.query.transformation.RewriteQueryTransformer;
 import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
-import org.springframework.ai.reader.EmptyJsonMetadataGenerator;
-import org.springframework.ai.reader.JsonMetadataGenerator;
-import org.springframework.ai.reader.JsonReader;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import pl.piomin.services.functions.stock.api.DailyStockData;
+import pl.piomin.services.functions.stock.api.Stock;
 import pl.piomin.services.functions.stock.api.StockData;
 
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 
@@ -37,26 +33,30 @@ import java.util.Map;
 @RequestMapping("/stocks")
 public class StockController {
 
+    private final ObjectMapper mapper = new ObjectMapper();
     private final static Logger LOG = LoggerFactory.getLogger(StockController.class);
     private final ChatClient chatClient;
+    private final RewriteQueryTransformer.Builder rqtBuilder;
+    private final RestTemplate restTemplate;
+    private final VectorStore store;
 
-    private VectorStore store;
+    @Value("${STOCK_API_KEY}")
+    private String apiKey;
 
     public StockController(ChatClient.Builder chatClientBuilder,
-                           VectorStore store) {
+                           VectorStore store,
+                           RestTemplate restTemplate) {
         this.chatClient = chatClientBuilder
                 .defaultAdvisors(new SimpleLoggerAdvisor())
                 .build();
+        this.rqtBuilder = RewriteQueryTransformer.builder()
+                .chatClientBuilder(chatClientBuilder);
         this.store = store;
+        this.restTemplate = restTemplate;
     }
 
-    @Autowired
-    RestTemplate restTemplate;
-    @Value("${STOCK_API_KEY}")
-    String apiKey;
-
-    @GetMapping("/load")
-    void load() {
+    @GetMapping("/load-data")
+    void load() throws JsonProcessingException {
         final List<String> companies = List.of("AAPL", "MSFT", "GOOG", "AMZN", "META", "NVDA");
         for (String company : companies) {
             StockData data = restTemplate.getForObject("https://api.twelvedata.com/time_series?symbol={0}&interval=1day&outputsize=10&apikey={1}",
@@ -67,25 +67,18 @@ public class StockController {
                 var list = data.getValues().stream().map(DailyStockData::getClose).toList();
                 var doc = Document.builder()
                         .id(company)
-                        .text(list.toString())
+                        .text(mapper.writeValueAsString(new Stock(company, list)))
                         .build();
                 store.add(List.of(doc));
                 LOG.info("Document added: {}", company);
             }
-//            var list = data.getValues().stream().map(DailyStockData::getClose).toList();
-//            Document.builder().id(company).text(list.toString());
-//            if (jsonResp != null) {
-//                Resource jsonResource = new ByteArrayResource(jsonResp.getBytes(StandardCharsets.UTF_8));
-//                List<Document> documents = new JsonReader(jsonResource, "values.close").get();
-//                store.add(documents);
-//            }
         }
     }
 
     @GetMapping("/docs")
     List<Document> query() {
         SearchRequest searchRequest = SearchRequest.builder()
-                .query("Similar trend to: [103.7, 100.4, 101.3, 102.8, 101.3, 98.6, 98.5, 100.8, 99.8, 100.0]")
+                .query("Find the most growth trends")
                 .topK(2)
                 .build();
         List<Document> docs = store.similaritySearch(searchRequest);
@@ -93,34 +86,42 @@ public class StockController {
         return docs;
     }
 
-    @RequestMapping("/find-best-trend")
+    @RequestMapping("/v1/most-growth-trend")
     String getBestTrend() {
         PromptTemplate pt = new PromptTemplate("""
-                Which of those market price trends is the most growth?
-                The first element in the list is the latest. 
+                {query}.
+                Which {target} is the most % growth?
+                The 0 element in the prices table is the latest price, while the last element is the oldest price.
                 """);
 
-        return this.chatClient.prompt(pt.create())
+        Prompt p = pt.create(Map.of("query", "Find the most growth trends", "target", "share"));
+
+        return this.chatClient.prompt(p)
                 .advisors(new QuestionAnswerAdvisor(store))
                 .call()
                 .content();
     }
 
-    @RequestMapping("/v2/find-best-trend")
+    @RequestMapping("/v2/most-growth-trend")
     String getBestTrendV2() {
         PromptTemplate pt = new PromptTemplate("""
-                Which of those market price trends is the most growth?
-                The first element in the list is the latest. 
+                {query}.
+                Which {target} is the most % growth?
+                The 0 element in the prices table is the latest price, while the last element is the oldest price.
                 """);
+
+        Prompt p = pt.create(Map.of("query", "Find the most growth trends", "target", "share"));
 
         Advisor retrievalAugmentationAdvisor = RetrievalAugmentationAdvisor.builder()
                 .documentRetriever(VectorStoreDocumentRetriever.builder()
-                        .similarityThreshold(0.5)
+                        .similarityThreshold(0.7)
+                        .topK(3)
                         .vectorStore(store)
                         .build())
+                .queryTransformers(rqtBuilder.promptTemplate(pt).build())
                 .build();
 
-        return this.chatClient.prompt(pt.create())
+        return this.chatClient.prompt(p)
                 .advisors(retrievalAugmentationAdvisor)
                 .call()
                 .content();
--- src/main/java/pl/piomin/services/functions/stock/api/Stock.java
+++ src/main/java/pl/piomin/services/functions/stock/api/Stock.java
@@ -1,9 +1,20 @@
 package pl.piomin.services.functions.stock.api;
 
+import java.util.List;
+
 public class Stock {
 
     private String name;
     private String symbol;
+    private List<String> prices;
+
+    public Stock() {
+    }
+
+    public Stock(String name, List<String> prices) {
+        this.prices = prices;
+        this.name = name;
+    }
 
     public String getName() {
         return name;
@@ -19,5 +30,13 @@ public class Stock {
 
     public void setSymbol(String symbol) {
         this.symbol = symbol;
+    }
+
+    public List<String> getPrices() {
+        return prices;
+    }
+
+    public void setPrices(List<String> prices) {
+        this.prices = prices;
     }
 }
