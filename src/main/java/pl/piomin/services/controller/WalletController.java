fdfdd952c9384eace54f9bc0c10c55e1a7b22258
piomin <piotr.minkowski@gmail.com>
2025-05-11 23:38:09 +0200

    Disable functions due to removal

--- src/main/java/pl/piomin/services/controller/WalletController.java
+++ src/main/java/pl/piomin/services/controller/WalletController.java
@@ -3,7 +3,7 @@ package pl.piomin.services.controller;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.prompt.PromptTemplate;
-import org.springframework.ai.model.function.FunctionCallingOptions;
+//import org.springframework.ai.model.function.FunctionCallingOptions;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,20 +31,20 @@ public class WalletController {
         this.walletTools = walletTools;
     }
 
-    @GetMapping
-    String calculateWalletValue() {
-        PromptTemplate pt = new PromptTemplate("""
-        What’s the current value in dollars of my wallet based on the latest stock daily prices ?
-        """);
-
-        return this.chatClient.prompt(pt.create(
-                FunctionCallingOptions.builder()
-                        .function("numberOfShares")
-                        .function("latestStockPrices")
-                        .build()))
-                .call()
-                .content();
-    }
+//    @GetMapping
+//    String calculateWalletValue() {
+//        PromptTemplate pt = new PromptTemplate("""
+//        What’s the current value in dollars of my wallet based on the latest stock daily prices ?
+//        """);
+//
+//        return this.chatClient.prompt(pt.create(
+//                FunctionCallingOptions.builder()
+//                        .function("numberOfShares")
+//                        .function("latestStockPrices")
+//                        .build()))
+//                .call()
+//                .content();
+//    }
 
     @GetMapping("/with-tools")
     String calculateWalletValueWithTools() {
